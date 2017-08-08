d1dd6b707c6da8f486f700c8b0ed542949f43183
Dawson M <dawson.mortenson@gmail.com>
2017-08-07 20:30:29 -0700

    add the correct, updated setup.rb file and make a few edits to the download site

--- setup.rb
+++ setup.rb
@@ -1,39 +1,54 @@
-#puts Dir.pwd
 puts 'installing cool atom packages'
+
+# This package autocloses tags
 system 'apm install autoclose-html'
+
+# This package displays the colors of RGB and Hex codes
 system 'apm install pigments'
+
+# This is an optional theme for atom that has neat icons for different file types
+# @@@IMPORTANT@@@ To enable the theme in atom you have to go to preferences -> themes -> change the UI Theme to 'Seti'
 system 'apm install seti-ui'
-Dir.chdir
-puts 'Your are working in the...'
-puts Dir.pwd
+
+
+Dir.chdir # I think this changes the working directory to the ~/
+# puts 'Your are working in the...'
+# puts Dir.pwd
+
+puts "Enter the first person's initials: "
+initials1 = gets.chomp
+puts "Enter the first person's full name: "
+human1 = gets.chomp
+puts "Enter the first person's email: "
+email1 = gets.chomp
+
+puts "Enter the second person's initials: "
+initials2 = gets.chomp
+puts "Enter the second person's full name: "
+human2 = gets.chomp
+puts "Enter the second person's email: "
+email2 = gets.chomp
+
+# Defines a var that creates and points to a .pairs file
 file = File.new('./.pairs', 'w')
-file.close
-puts 'installing atom packages....'
-puts 'apm install autoclose-html'
-puts 'apm install seti-ui'
-puts 'creating .pairs file....'
-puts 'touch ~/.pairs'
-# puts 'filling .pairs file....'
-# puts 'echo "pairs:" >> ~/.pairs'
-# puts 'echo "  dm: Dawson Mortenson" >> ~/.pairs'
-# puts 'echo "  edlf: Enrique de la Fuente" >> ~/.pairs'
-# puts 'echo "email:" >> ~/.pairs'
-# puts 'echo "  dm: dawson.mortenson@gmail.com" >> ~/.pairs'
-# puts 'echo "  edlf: queenrique@gmail.com" >> ~/.pairs'
-# puts 'done...'
+
+# file.close
+# puts 'creating .pairs file....'
+# puts 'touch ~/.pairs'
+
 puts 'opening the .pairs file....'
 target = open(file, 'w')
 
 puts 'filling the .pairs file....'
 target.write("pairs:")
 target.write("\n")
-target.write("  dm: Dawson Mortenson")
+target.write("  #{initials1}: #{human1}")
 target.write("\n")
-target.write("  edlf: Enrique de la Fuente")
+target.write("  #{initials2}: #{human2}")
 target.write("\n")
 target.write("email:")
 target.write("\n")
-target.write("  dm: dawson.mortenson@gmail.com")
+target.write("  #{initials1}: #{email1}")
 target.write("\n")
-target.write("  edlf: queenrique@gmail.com")
+target.write("  #{initials2}: #{email2}")
 puts 'done....'
